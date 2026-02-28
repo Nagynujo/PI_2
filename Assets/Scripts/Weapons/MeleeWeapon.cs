f77fe0dab0a9c98237d9890e9106e954f30b95c7
Nagy <nagya18101939@gmail.com>
2026-02-28 18:25:16 -0300

    Adicionado script de arma melee e inicio do inimigo - Unity project

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MeleeWeapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange = 0.5f;
+    [SerializeField] private int attackDamage = 1;
+    [SerializeField] private float attackCooldown = 0.4f;
 
-    // Update is called once per frame
+   
+    [SerializeField] private LayerMask enemyLayer;
+
+    
+    [SerializeField] private bool showCooldownDebug = true;
+
+    private float nextAttackTime;
+    private bool wasOnCooldown;
+
     void Update()
     {
-        
+        bool isOnCooldown = Time.time < nextAttackTime;
+
+        // Debug limpo
+        if (showCooldownDebug && isOnCooldown != wasOnCooldown)
+        {
+            Debug.Log(isOnCooldown ? "Cooldown iniciado" : "Ataque pronto!");
+            wasOnCooldown = isOnCooldown;
+        }
+
+        // Mouse esquerdo
+        if (Mouse.current.leftButton.wasPressedThisFrame && !isOnCooldown)
+        {
+            Attack();
+            nextAttackTime = Time.time + attackCooldown;
+        }
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(
+            attackPoint.position,
+            attackRange,
+            enemyLayer
+        );
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.SendMessage(
+                "TakeDamage",
+                attackDamage,
+                SendMessageOptions.DontRequireReceiver
+            );
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
