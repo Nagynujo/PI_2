45340fac8edc2be5189483bf0cbf422c89f483f0
Nagy <nagya18101939@gmail.com>
2026-03-02 23:54:32 -0300

    Atualização de Input - Unity project

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -3,33 +3,77 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    private float horizontalInput;
     private Rigidbody2D rb;
 
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private float horizontalInput;
+    private bool isGrounded;
+
     [SerializeField] int velocity = 4;
+    [SerializeField] float jumpForce = 300f;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundLayer;
-    private bool isGrounded;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        
+        moveAction = new InputAction(
+            "Move",
+            InputActionType.Value,
+            "<Keyboard>/leftArrow"
+        );
+        moveAction.AddCompositeBinding("2DVector")
+            .With("Left", "<Keyboard>/a")
+            .With("Left", "<Keyboard>/leftArrow")
+            .With("Right", "<Keyboard>/d")
+            .With("Right", "<Keyboard>/rightArrow");
+
+        // Pulo
+        jumpAction = new InputAction(
+            "Jump",
+            InputActionType.Button,
+            "<Keyboard>/space"
+        );
+    }
+
+    void OnEnable()
+    {
+        moveAction.Enable();
+        jumpAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+       
+        horizontalInput = moveAction.ReadValue<Vector2>().x;
+
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            0.2f,
+            groundLayer
+        );
+
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
         {
-            rb.AddForce(Vector2.up * 300);
+            rb.AddForce(Vector2.up * jumpForce);
         }
-
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput * velocity, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(
+            horizontalInput * velocity,
+            rb.linearVelocity.y
+        );
     }
 }
